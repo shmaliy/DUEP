dc50f7c5f515927766f072affe1517a42b7a4417
Maxim Tumanov <maxim.tymanov@yandex.ua>
2012-07-18 13:07:53 +0300

    Контроллеры для фронта

---
+++ application/modules/contents/controllers/AwardsController.php
@@ -0,0 +1,166 @@
+<?php
+
+class Contents_NewsController extends Zend_Controller_Action
+{	
+	/**
+	 * Prepare controller for work with ajax based requests
+	 * 
+	 * (non-PHPdoc)
+	 * @see Zend_Controller_Action::init()
+	 */
+	public function init()
+	{
+		$request = $this->getRequest();
+		
+		if ($request->isXmlHttpRequest() || $request->isPost()) {
+			// Both html or json ajax responses need to disable layout
+        	$this->_helper->layout()->disableLayout();			
+		}
+		
+		// Add actions wich can work with ajax
+		$context = $this->_helper->AjaxContext();
+		$context->addActionContext('index', 'json');
+		$context->addActionContext('config', 'json');
+		$context->addActionContext('front-announcements', 'json');
+		$context->addActionContext('front-news', 'json');
+		
+		$context->initContext('json');
+	}
+	
+	/**
+	 * Main page controller
+	 * Generate questions form and procces it if ajax requested
+	 */
+	public function indexAction()
+    {
+    	
+    	
+    	$groupsMapper = new Contents_Model_Mapper_ContentsGroups();
+    	$this->view->agroup = $groupsMapper->getFrontGroupByAlias ("announcements");
+    	$this->view->ngroup = $groupsMapper->getFrontGroupByAlias ("news");
+    	$this->view->egroup = $groupsMapper->getFrontGroupByAlias ("events");
+    	 
+    	$catMapper = new Contents_Model_Mapper_ContentsCategories();
+    	$this->view->acats = $catMapper->getFrontCatsByGroupId($this->view->agroup->id);
+    	$this->view->ncats = $catMapper->getFrontCatsByGroupId($this->view->ngroup->id);
+    	$this->view->ecats = $catMapper->getFrontCatsByGroupId($this->view->egroup->id);
+    	 
+    	$contentsMapper = new Contents_Model_Mapper_Contents();
+    	$this->view->events = $contentsMapper->getFrontContentsByGroupId($this->view->agroup->id,'date_created desc');
+    	$this->view->announcements = $contentsMapper->getFrontContentsByGroupId($this->view->agroup->id,'date_created desc');
+    	$this->view->news = $contentsMapper->getFrontContentsByGroupId($this->view->ngroup->id,'date_created desc');
+    	
+    	    	
+    	$translatedMonths = array(
+	    	1 => 'Январь',
+	    	2 => 'Февраль',
+	    	3 => 'Март',
+	    	4 => 'Апрель',
+	    	5 => 'Май',
+	    	6 => 'Июнь',
+	    	7 => 'Июль',
+	    	8 => 'Август',
+	    	9 => 'Сентябрь',
+	    	10 => 'Октябрь',
+	    	11 => 'Ноябрь',
+	    	12 => 'Декабрь'
+    	);
+    	
+    	$this->view->announcements->formatDate('date_created', $translatedMonths, 'г.');
+    	$this->view->events->formatDate('date_created', $translatedMonths, 'г.');
+    	$this->view->news->formatDate('date_created', $translatedMonths, 'г.');
+    }
+    public function viewAction()
+    {
+    	 
+    	$alias =  $this->getRequest()->getParam('alias');
+    	
+    	$groupsMapper = new Contents_Model_Mapper_ContentsGroups();
+    	$this->view->agroup = $groupsMapper->getFrontGroupByAlias ("announcements");
+    	$this->view->ngroup = $groupsMapper->getFrontGroupByAlias ("news");
+    	$this->view->egroup = $groupsMapper->getFrontGroupByAlias ("events");
+    	 
+    	$catMapper = new Contents_Model_Mapper_ContentsCategories();
+    	$this->view->acats = $catMapper->getFrontCatsByGroupId($this->view->agroup->id);
+    	$this->view->ncats = $catMapper->getFrontCatsByGroupId($this->view->ngroup->id);
+    	$this->view->ecats = $catMapper->getFrontCatsByGroupId($this->view->egroup->id);
+    	 
+    	$contentsMapper = new Contents_Model_Mapper_Contents();
+    	$this->view->events = $contentsMapper->getFrontContentsByGroupId($this->view->agroup->id,'date_created desc');
+    	$this->view->announcements = $contentsMapper->getFrontContentsByGroupId($this->view->agroup->id,'date_created desc');
+    	$this->view->news = $contentsMapper->getFrontContentsByGroupId($this->view->ngroup->id,'date_created desc');
+    	
+    	$contentMapper = new Contents_Model_Mapper_Contents();
+    	$this->view->one_news = $contentMapper->getFrontContentByAlias($alias);
+    	 
+    	$translatedMonths = array(
+    	1 => 'Январь',
+    	2 => 'Февраль',
+    	3 => 'Март',
+    	4 => 'Апрель',
+    	5 => 'Май',
+    	6 => 'Июнь',
+    	7 => 'Июль',
+    	8 => 'Август',
+    	9 => 'Сентябрь',
+    	10 => 'Октябрь',
+    	11 => 'Ноябрь',
+    	12 => 'Декабрь'
+    	);
+    	 
+    	$this->view->one_news->formatDate('date_created', $translatedMonths, 'г.');
+    	$this->view->announcements->formatDate('date_created', $translatedMonths, 'г.');
+    	$this->view->events->formatDate('date_created', $translatedMonths, 'г.');
+    	$this->view->news->formatDate('date_created', $translatedMonths, 'г.');
+    }
+    
+    /**
+     * ЭТОТ МЕТОД СОЗДАН ДЛЯ НАДИ ЧТОБ ОНА МОГЛА ВЕРСТАТЬ ПРИМЕНИТЕЛЬНО К САЙТУ - НЕ УДАЛЯТЬ
+     */
+    public function developAction()
+    {
+    	
+    }
+	
+    /**
+     * Config page
+     */
+	public function configAction()
+    {
+		$this->_helper->layout->setLayout('admin-layout');
+    	$request = $this->getRequest();
+    	$formAdminConfig = new Application_Form_AdminConfig();
+    	$adminConfig = new Application_Model_AdminConfig();
+    	
+    	if ($request->isXmlHttpRequest() || $request->isPost()) {
+    		if ($formAdminConfig->isValid($request->getParams())) {
+    			$adminConfig->save($formAdminConfig->getValues());
+    			$this->view->error = $formAdminConfig->getValues();
+    		} else {
+    			$this->view->error = $formAdminConfig->getErrors();
+    		}
+    	} else {
+    		$config = $adminConfig->load();
+    		unset($config['demo']); // forse demo off after sucess config
+    		$this->view->formAdminConfig = $formAdminConfig->setDefaults($config);
+    	}
+    }
+    
+    public function usersAction()
+    {
+    	
+    }
+    public function frontAnnouncementsAction()
+    {
+    	$ans_id = $this->getRequest()->getParam('ans_id');
+    	$contentsMapper = new Contents_Model_Mapper_Contents();
+    	$this->view->contents = $contentsMapper->getFrontContentsByCatId($ans_id,'date_created desc',6)->toArray();
+    }
+    public function frontNewsAction()
+    {
+    	$news_id = $this->getRequest()->getParam('news_id');
+    	$contentsMapper = new Contents_Model_Mapper_Contents();
+    	$this->view->contents = $contentsMapper->getFrontContentsByCatId($news_id,'date_created desc',6)->toArray();
+    }
+    
+}
---
+++ application/modules/contents/controllers/SubdivisionController.php
@@ -0,0 +1,106 @@
+<?php
+
+class Contents_DirectionsController extends Zend_Controller_Action
+{	
+	/**
+	 * Prepare controller for work with ajax based requests
+	 * 
+	 * (non-PHPdoc)
+	 * @see Zend_Controller_Action::init()
+	 */
+	public function init()
+	{
+		$request = $this->getRequest();
+		
+		if ($request->isXmlHttpRequest() || $request->isPost()) {
+			// Both html or json ajax responses need to disable layout
+        	$this->_helper->layout()->disableLayout();			
+		}
+		
+		// Add actions wich can work with ajax
+		$context = $this->_helper->AjaxContext();
+		$context->addActionContext('index', 'json');
+		$context->addActionContext('config', 'json');
+		
+		$context->initContext('json');
+	}
+	
+	/**
+	 * Main page controller
+	 * Generate questions form and procces it if ajax requested
+	 */
+	public function indexAction()
+    {
+    	
+    }
+    public function viewAction()
+    {
+   // 	"directions/:direction_name" 
+    	
+    }
+    public function historyAction()
+    {
+    //	"directions/:direction_name/history"
+    	 
+    }
+    public function disciplinesAction()
+    {
+    //	"directions/:direction_name/disciplines"
+    	 
+    }
+    public function disciplineAction()
+    {
+     //   	"directions/:direction_name/disciplines/:discipline_name"
+        	 
+    }
+    
+    /**
+     * ЭТОТ МЕТОД СОЗДАН ДЛЯ НАДИ ЧТОБ ОНА МОГЛА ВЕРСТАТЬ ПРИМЕНИТЕЛЬНО К САЙТУ - НЕ УДАЛЯТЬ
+     */
+    public function developAction()
+    {
+    	
+    }
+	
+    /**
+     * Config page
+     */
+	public function configAction()
+    {
+		$this->_helper->layout->setLayout('admin-layout');
+    	$request = $this->getRequest();
+    	$formAdminConfig = new Application_Form_AdminConfig();
+    	$adminConfig = new Application_Model_AdminConfig();
+    	
+    	if ($request->isXmlHttpRequest() || $request->isPost()) {
+    		if ($formAdminConfig->isValid($request->getParams())) {
+    			$adminConfig->save($formAdminConfig->getValues());
+    			$this->view->error = $formAdminConfig->getValues();
+    		} else {
+    			$this->view->error = $formAdminConfig->getErrors();
+    		}
+    	} else {
+    		$config = $adminConfig->load();
+    		unset($config['demo']); // forse demo off after sucess config
+    		$this->view->formAdminConfig = $formAdminConfig->setDefaults($config);
+    	}
+    }
+    
+    public function usersAction()
+    {
+    	
+    }
+    public function frontAnnouncementsAction()
+    {
+    	$ans_id = $this->getRequest()->getParam('ans_id');
+    	$contentsMapper = new Contents_Model_Mapper_Contents();
+    	$this->view->contents = $contentsMapper->getFrontContentsByCatId($ans_id,'date_created desc',6)->toArray();
+    }
+    public function frontNewsAction()
+    {
+    	$news_id = $this->getRequest()->getParam('news_id');
+    	$contentsMapper = new Contents_Model_Mapper_Contents();
+    	$this->view->contents = $contentsMapper->getFrontContentsByCatId($news_id,'date_created desc',6)->toArray();
+    }
+    
+}
