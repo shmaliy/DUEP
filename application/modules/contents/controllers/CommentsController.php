3c854e35130cf607a970ef262f04f0de0a2df67a
Maxim Tumanov <maxim.tymanov@yandex.ua>
2012-08-16 10:19:10 +0300

    +

---
+++ application/modules/contents/controllers/CommentsController.php
@@ -0,0 +1,90 @@
+<?php
+
+class Contents_ContactsController extends Zend_Controller_Action
+{	
+	/**
+	 * Prepare controller for work with ajax based requests
+	 * 
+	 * (non-PHPdoc)
+	 * @see Zend_Controller_Action::init()
+	 */
+    protected $_lang;
+	public function init()
+	{
+		$request = $this->getRequest();
+		
+		if ($request->isXmlHttpRequest() || $request->isPost()) {
+			// Both html or json ajax responses need to disable layout
+        	$this->_helper->layout()->disableLayout();			
+		}
+		
+		// Add actions wich can work with ajax
+		$context = $this->_helper->AjaxContext();
+		$context->addActionContext('index', 'json');
+		$context->addActionContext('config', 'json');
+
+		
+		$context->initContext('json');
+		$this->_lang = Zend_Registry::get('lang');
+	}
+	
+	/**
+	 * Обработчик страницы "Контакты"
+	 */
+	public function indexAction()
+    {
+
+       
+    }
+    
+    
+    /**
+     * ЭТОТ МЕТОД СОЗДАН ДЛЯ НАДИ ЧТОБ ОНА МОГЛА ВЕРСТАТЬ ПРИМЕНИТЕЛЬНО К САЙТУ - НЕ УДАЛЯТЬ
+     */
+    public function developAction()
+    {
+    	
+    }
+	
+    /**
+     * Config page
+     */
+	public function configAction()
+    {
+		$this->_helper->layout->setLayout('admin-layout');
+    	$request = $this->getRequest();
+    	$formAdminConfig = new Application_Form_AdminConfig();
+    	$adminConfig = new Application_Model_AdminConfig();
+    	
+    	if ($request->isXmlHttpRequest() || $request->isPost()) {
+    		if ($formAdminConfig->isValid($request->getParams())) {
+    			$adminConfig->save($formAdminConfig->getValues());
+    			$this->view->error = $formAdminConfig->getValues();
+    		} else {
+    			$this->view->error = $formAdminConfig->getErrors();
+    		}
+    	} else {
+    		$config = $adminConfig->load();
+    		unset($config['demo']); // forse demo off after sucess config
+    		$this->view->formAdminConfig = $formAdminConfig->setDefaults($config);
+    	}
+    }
+    
+    public function usersAction()
+    {
+    	
+    }
+    public function frontAnnouncementsAction()
+    {
+    	$ans_id = $this->getRequest()->getParam('ans_id');
+    	$contentsMapper = new Contents_Model_Mapper_Contents();
+    	$this->view->contents = $contentsMapper->getFrontContentsByCatId($ans_id,'date_created desc',6)->toArray();
+    }
+    public function frontNewsAction()
+    {
+    	$news_id = $this->getRequest()->getParam('news_id');
+    	$contentsMapper = new Contents_Model_Mapper_Contents();
+    	$this->view->contents = $contentsMapper->getFrontContentsByCatId($news_id,'date_created desc',6)->toArray();
+    }
+    
+}
